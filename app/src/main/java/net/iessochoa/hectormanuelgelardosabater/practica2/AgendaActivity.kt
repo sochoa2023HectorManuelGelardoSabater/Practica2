76e4be9813ae0e455ea21b07a58a31cfcbecbd23
Hector <hectormanuel.gs@hotmail.com>
2023-11-02 11:23:03 +0100

    EditText puestos

---
+++ app/src/main/java/net/iessochoa/hectormanuelgelardosabater/practica2/AgendaActivity.kt
@@ -0,0 +1,11 @@
+package net.iessochoa.hectormanuelgelardosabater.practica2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AgendaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agenda)
+    }
+}
