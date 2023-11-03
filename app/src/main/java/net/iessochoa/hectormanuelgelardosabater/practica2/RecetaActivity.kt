199b1b31526d2a49421e0c009072e0f7543a8021
Hector <hectormanuel.gs@hotmail.com>
2023-11-03 10:17:08 +0100

    Imagen insertada

---
+++ app/src/main/java/net/iessochoa/hectormanuelgelardosabater/practica2/RecetaActivity.kt
@@ -0,0 +1,11 @@
+package net.iessochoa.hectormanuelgelardosabater.practica2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecetaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_receta)
+    }
+}
